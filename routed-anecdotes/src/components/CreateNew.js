24f3714ebca9ecd431533804fe64c37c891888dd
Dennis Leung <artyistic@gmail.com>
2023-08-28 20:28:58 -0400

    7.4-7.6

--- routed-anecdotes/src/components/CreateNew.js
+++ routed-anecdotes/src/components/CreateNew.js
@@ -1,24 +1,36 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../hooks"
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  // const [content, setContent] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [info, setInfo] = useState('')
+
+  const content = useField("text")
+  const author = useField("text")
+  const info = useField("text")
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    props.setNotification(`a new anecdote ${content} created!`)
+    props.setNotification(`a new anecdote ${content.value} created!`)
     setTimeout(() => props.setNotification(""), 5000)
     navigate("/")
   }
+
+  const resetAllFields = () => {
+    content.resetValue()
+    author.resetValue()
+    info.resetValue()
+  }
+
+//  {...(({resetValue, ...rest}) => rest)(content)} creates a new object excluding the resetValue attribute of the useField object 
 
   return (
     <div>
@@ -26,17 +38,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...(({resetValue, ...rest}) => rest)(content)} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...(({resetValue, ...rest}) => rest)(author)}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...(({resetValue, ...rest}) => rest)(info)} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={resetAllFields}>reset</button>
       </form>
     </div>
   )
